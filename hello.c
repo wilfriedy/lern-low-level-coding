3ad467b1db70d6555fd8e7e44dda4abe28fe8bd6
wilfriedy <wilfriedakpe3@gmail.com>
2025-05-24 22:06:52 +0100

    first c commit

---
+++ hello.c
@@ -0,0 +1,12 @@
+/* hello.c */
+
+#include <stdio.h>
+
+int main ()
+{
+
+printf ("hello world\n");
+
+return 0;
+
+} 
---
+++ name.c
@@ -0,0 +1,11 @@
+/* name.c */
+#include <stdio.h>
+
+int main()
+{
+char name[32];
+printf("what is your name\n");
+scanf("%s", name);
+printf("Hello %s\n", name);
+return 0;
+}
