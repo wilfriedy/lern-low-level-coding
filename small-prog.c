22e6ffa49061eeecca978cff218c5e72af790585
wilfriedy <wilfriedakpe3@gmail.com>
2025-05-25 18:45:02 +0100

    small calculator in c

---
+++ small-prog.c
@@ -0,0 +1,19 @@
+/*small prog*/
+
+#include <stdio.h>
+
+int main ()
+{
+int x, y, answer;
+printf("Choose a number\n");
+scanf("%d", &x);
+printf("Choose another num\n");
+scanf("%d", &y);
+answer = x + y;
+printf("the answer is %d\n", answer);
+return 0;
+
+
+
+}
+
